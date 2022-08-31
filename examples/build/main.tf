c68d59ad51e84caca73212b87d50aea6fcde1fc2
Beauregard, Josh <sanguis@users.noreply.github.com>
2022-08-31 10:39:02 -0400

    Adding instance private address to database allow list
    Also moved example tf ops to versions.tf

--- examples/build/main.tf
+++ examples/build/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.1"
-  required_providers {
-    linode = {
-      source  = "linode/linode"
-      version = ">= 1.28"
-    }
-    random = ">= 2.0"
-  }
-}
 provider "linode" {
 }
 
